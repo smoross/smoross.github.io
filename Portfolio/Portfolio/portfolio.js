6cabb7a24c9b2895094474db54192cafdfd5a015
smoross.github.io <smoross@umich.edu>
2018-04-06 11:52:23 -0400

    Add files via upload

---
+++ Portfolio/Portfolio/portfolio.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+$(function(){
+    var $page = jQuery.url.attr("file");
+    $('ul.navigation li a').each(function(){
+        var $href = $(this).attr('href');
+        if ( ($href == $page) || ($href == '') ) {
+            $(this).addClass('on');
+        } else {
+            $(this).removeClass('on');
+        }
+    });
+});
+</script>
